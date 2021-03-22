d9fa93bdf54941b0a3283e852bed966ab0203c0f
Constantinos Giannacoulis <c.giannacoulis@codehub.gr>
2021-03-22 11:43:55 +0200

    Added logging mechanism using SLF4J and Log4j2 implementation.
    Signed-off-by: Constantinos Giannacoulis <c.giannacoulis@codehub.gr>

--- src/main/java/org/example/database/DbDemo.java
+++ src/main/java/org/example/database/DbDemo.java
@@ -3,6 +3,8 @@ package org.example.database;
 import com.thedeanda.lorem.Lorem;
 import com.thedeanda.lorem.LoremIpsum;
 import org.h2.tools.Server;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 import java.io.InputStream;
@@ -19,6 +21,7 @@ import java.util.concurrent.ThreadLocalRandom;
 import static java.lang.System.exit;
 
 public class DbDemo {
+	private static final Logger logger = LoggerFactory.getLogger(DbDemo.class);
 	private static final String DB_URL = "jdbc:h2:mem:sample";
 	private static final String DB_USERNAME = "sa";
 	private static final String DB_PASSWORD = "";
@@ -45,7 +48,7 @@ public class DbDemo {
 		demo.batchInsertData(connection);
 		demo.selectData(connection);
 
-		demo.enableTransationMode(connection);
+		demo.enableTransactionMode(connection);
 
 		// Update data
 		demo.updateData(connection);
@@ -66,12 +69,115 @@ public class DbDemo {
 		demo.stopH2Server();
 	}
 
-	private void enableTransationMode(Connection connection) {
+	private void loadSqlCommands() {
+		try (InputStream inputStream = DbDemo.class.getClassLoader().getResourceAsStream("sql.properties")) {
+			if (inputStream == null) {
+				logger.error("Unable to load SQL commands.");
+				exit(-1);
+			}
+			sqlCommands.load(inputStream);
+		} catch (IOException e) {
+			logger.error("Error while loading SQL commands.", e);
+		}
+	}
+
+	private void startH2Server() {
+		try {
+			h2Server = Server.createTcpServer("-tcpAllowOthers", "-tcpDaemon");
+			h2Server.start();
+			logger.info("H2 Database server is now accepting connections.");
+		} catch (SQLException throwables) {
+			logger.error("Unable to start H2 database server.", throwables);
+			exit(-1);
+		}
+	}
+
+	private Connection registerDatabaseDriver() {
+		Connection connection = null;
+		try {
+			connection = DriverManager.getConnection(DB_URL, DB_USERNAME, DB_PASSWORD);
+		} catch (SQLException throwables) {
+			logger.error("Unable to get a connection to H2 database server.", throwables);
+			exit(-1);
+		}
+		return connection;
+	}
+
+	private void createTable(Connection connection) {
+		try (Statement statement = connection.createStatement()) {
+			int resultRows = statement.executeUpdate(sqlCommands.getProperty("create.table.001"));
+
+			logger.debug("Statement returned {}.", resultRows);
+		} catch (SQLException throwables) {
+			logger.error("Unable to create target database table.", throwables);
+		}
+	}
+
+	private void insertData(Connection connection) {
+		try (Statement statement = connection.createStatement()) {
+			int resultRows = statement.executeUpdate(sqlCommands.getProperty("insert.table.001"));
+			logger.debug("Statement returned {}.", resultRows);
+			resultRows = statement.executeUpdate(sqlCommands.getProperty("insert.table.002"));
+			logger.debug("Statement returned {}.", resultRows);
+			resultRows = statement.executeUpdate(sqlCommands.getProperty("insert.table.003"));
+			logger.debug("Statement returned {}.", resultRows);
+			resultRows = statement.executeUpdate(sqlCommands.getProperty("insert.table.004"));
+			logger.debug("Statement returned {}.", resultRows);
+			resultRows = statement.executeUpdate(sqlCommands.getProperty("insert.table.005"));
+			logger.debug("Statement returned {}.", resultRows);
+
+		} catch (SQLException throwables) {
+			logger.error("Error occurred while inserting data.", throwables);
+		}
+	}
+
+	private void batchInsertData(Connection connection) {
+		try (PreparedStatement preparedStatement = connection.prepareStatement(
+				sqlCommands.getProperty("insert.table.000"))) {
+			generateData(preparedStatement, 1000);
+
+			int[] affectedRows = preparedStatement.executeBatch();
+			logger.debug("Rows inserted {}.", Arrays.stream(affectedRows).sum());
+
+		} catch (SQLException throwables) {
+			logger.error("Error occurred while batch inserting data.", throwables);
+		}
+	}
+
+	private void selectData(Connection connection) {
+		try (Statement statement = connection.createStatement(); ResultSet resultSet = statement.executeQuery(
+				sqlCommands.getProperty("select.table.001"))) {
+
+			while (resultSet.next()) {
+				//@formatter:off
+				logger.info("id:{}, firstName:{}, lastName:{}, age:{}.",
+							resultSet.getLong("ID"),
+							resultSet.getString("FIRSTNAME"),
+							resultSet.getString("LASTNAME"),
+							resultSet.getInt("AGE"));
+				//@formatter:on
+			}
+		} catch (SQLException throwables) {
+			logger.error("Error occurred while retrieving data", throwables);
+		}
+	}
+
+	private void enableTransactionMode(Connection connection) {
 		try {
 			connection.setAutoCommit(false);
 		} catch (SQLException throwables) {
-			System.out.println("Unable to enable transaction mode");
-			throwables.printStackTrace();
+			logger.warn("Unable to persist/rollback transaction.", throwables);
+		}
+	}
+
+	private void updateData(Connection connection) {
+		try (Statement statement = connection.createStatement()) {
+			int resultRows = statement.executeUpdate(sqlCommands.getProperty("update.table.001"));
+
+			logger.debug("Rows updated {}.", resultRows);
+
+		} catch (SQLException throwables) {
+			logger.error("Error occurred while updating data.", throwables);
 		}
 	}
 
@@ -83,114 +189,7 @@ public class DbDemo {
 				connection.rollback();
 			}
 		} catch (SQLException throwables) {
-			System.out.println("Unable to enable transaction mode");
-			throwables.printStackTrace();
-		}
-	}
-
-	private void loadSqlCommands() {
-		try (InputStream inputStream = DbDemo.class.getClassLoader().getResourceAsStream("sql.properties")) {
-			if (inputStream == null) {
-				System.err.println("Unable to load SQL commands");
-				exit(-1);
-			}
-			sqlCommands.load(inputStream);
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
-
-	private void startH2Server() {
-		try {
-			h2Server = Server.createTcpServer("-tcpAllowOthers", "-tcpDaemon");
-			h2Server.start();
-			System.out.println("H2 Database server is now accepting connections.");
-		} catch (SQLException throwables) {
-			System.err.println("Unable to start H2 database server.");
-			throwables.printStackTrace();
-			exit(-1);
-		}
-	}
-
-	private Connection registerDatabaseDriver() {
-		Connection connection = null;
-		try {
-			connection = DriverManager.getConnection(DB_URL, DB_USERNAME, DB_PASSWORD);
-		} catch (SQLException throwables) {
-			System.err.println("Unable to get a connection from H2 database server.");
-			throwables.printStackTrace();
-			exit(-1);
-		}
-		return connection;
-	}
-
-	private void createTable(Connection connection) {
-		try (Statement statement = connection.createStatement()) {
-			int resultRows = statement.executeUpdate(sqlCommands.getProperty("create.table.001"));
-
-			System.out.println("Statement returned " + resultRows);
-		} catch (SQLException throwables) {
-			throwables.printStackTrace();
-		}
-	}
-
-	private void insertData(Connection connection) {
-		try (Statement statement = connection.createStatement()) {
-			int resultRows = statement.executeUpdate(sqlCommands.getProperty("insert.table.001"));
-			System.out.println("Statement returned " + resultRows);
-			resultRows = statement.executeUpdate(sqlCommands.getProperty("insert.table.002"));
-			System.out.println("Statement returned " + resultRows);
-			resultRows = statement.executeUpdate(sqlCommands.getProperty("insert.table.003"));
-			System.out.println("Statement returned " + resultRows);
-			resultRows = statement.executeUpdate(sqlCommands.getProperty("insert.table.004"));
-			System.out.println("Statement returned " + resultRows);
-			resultRows = statement.executeUpdate(sqlCommands.getProperty("insert.table.005"));
-			System.out.println("Statement returned " + resultRows);
-
-		} catch (SQLException throwables) {
-			System.err.println("Error occurred while inserting data");
-			throwables.printStackTrace();
-		}
-	}
-
-	private void batchInsertData(Connection connection) {
-		try (PreparedStatement preparedStatement = connection.prepareStatement(
-				sqlCommands.getProperty("insert.table.000"))) {
-			generateData(preparedStatement, 1000);
-
-			int[] affectedRows = preparedStatement.executeBatch();
-			System.out.println("Rows inserted " + Arrays.stream(affectedRows).sum());
-
-		} catch (SQLException throwables) {
-			System.err.println("Error occurred while retrieving data");
-			throwables.printStackTrace();
-		}
-	}
-
-	private void selectData(Connection connection) {
-		try (Statement statement = connection.createStatement(); ResultSet resultSet = statement.executeQuery(
-				sqlCommands.getProperty("select.table.001"))) {
-
-			while (resultSet.next()) {
-				System.out.println("id:" + resultSet.getLong("ID") + ", firstName:" + resultSet.getString("FIRSTNAME") +
-										   ", lastName:" + resultSet.getString("LASTNAME") + ", age:" +
-										   resultSet.getInt("AGE"));
-			}
-		} catch (SQLException throwables) {
-			System.err.println("Error occurred while retrieving data");
-			throwables.printStackTrace();
-		}
-	}
-
-	private void updateData(Connection connection) {
-		try (Statement statement = connection.createStatement()) {
-			int resultRows = statement.executeUpdate(sqlCommands.getProperty("update.table.001"));
-
-			System.out.println("Rows updated " + resultRows);
-
-		} catch (SQLException throwables) {
-			System.err.println("Error occurred while updating data");
-			throwables.printStackTrace();
+			logger.warn("Unable to enable transaction mode.", throwables);
 		}
 	}
 
@@ -198,11 +197,10 @@ public class DbDemo {
 		try (Statement statement = connection.createStatement()) {
 			int resultRows = statement.executeUpdate(sqlCommands.getProperty("delete.table.001"));
 
-			System.out.println("Rows updated " + resultRows);
+			logger.debug("Rows updated {}.", resultRows);
 
 		} catch (SQLException throwables) {
-			System.err.println("Error occurred while updating data");
-			throwables.printStackTrace();
+			logger.error("Error occurred while updating data.", throwables);
 		}
 	}
 
@@ -214,7 +212,7 @@ public class DbDemo {
 		if (h2Server.isRunning(true)) {
 			h2Server.stop();
 			h2Server.shutdown();
-			System.out.println("H2 Database server has been shutdown.");
+			logger.info("H2 Database server has been shutdown.");
 		}
 	}
 
